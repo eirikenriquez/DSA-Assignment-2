0405aa3b1e697ec4c98b3036c922d7ca4816197d
Eirik Enriquez <eirikdbbd@gmail.com>
2022-09-27 06:28:06 +1300

    Initial commit

---
+++ Question1/src/dsa/assignment/pkg2/DSAAssignment2.java
@@ -0,0 +1,16 @@
+package dsa.assignment.pkg2;
+
+/**
+ *
+ * @author eirik
+ */
+public class DSAAssignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
