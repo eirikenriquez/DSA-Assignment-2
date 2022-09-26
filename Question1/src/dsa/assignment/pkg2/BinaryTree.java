6dc360173a6319134e9accf159c6aab9582a3f76
Eirik Enriquez <eirikdbbd@gmail.com>
2022-09-27 11:38:26 +1300

    asddsa

--- Question1/src/dsa/assignment/pkg2/BinaryTree.java
+++ Question1/src/dsa/assignment/pkg2/BinaryTree.java
@@ -57,23 +57,44 @@ public class BinaryTree<E extends Comparable> {
     }
 
     /**
+     * This method finds and returns the Node in the binary tree that contains
+     * the data given by the argument. This method returns the root Node if it
+     * contains the same data as the argument. Otherwise, it runs a private
+     * recursive method that traverses through the tree to find the data.
      *
-     * @param data
-     * @return
+     * @param data This is a comparable object that is stored in a Node.
+     * @return Returns the data object if it finds the Node. Otherwise, returns
+     * null.
      */
     public E findNode(E data) {
+        if (root == null) {
+            return null;
+        }
+
         if (root.data.compareTo(data) == 0) {
             return data;
         } else {
             return findNode(data, root);
         }
+
     }
 
+    /**
+     * This method recursively searches the binary tree and returns the data if
+     * a Node contains the same data. Otherwise, returns null.
+     *
+     * @param toFind Comparable object that this method searches for.
+     * @param current Node object that is the current key for traversing through
+     * the binary tree.
+     * @return
+     */
     private E findNode(E toFind, Node current) {
-        if (current.data.compareTo(toFind) > 0) {
+        if (current == null) {
+            return null;
+        } else if (current.data.compareTo(toFind) > 0) {
             findNode(toFind, current.left);
         } else if (current.data.compareTo(toFind) < 0) {
-
+            findNode(toFind, current.right);
         } else {
             return toFind;
         }
